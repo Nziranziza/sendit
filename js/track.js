158a76da14dc86e88908924701b99aa61c85d2b0
Nziranziza <nziranzizadaniel@gmail.com>
2018-11-15 12:44:17 +0200

    made some modification on tracking page

--- js/track.js
+++ js/track.js
@@ -6,7 +6,7 @@ function initMap() {
     // The map, centered at Uluru
     var map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: kamonyi});
    // The marker, positioned at Uluru
-    var present_location = new google.maps.Marker({position: kamonyi, map: map,title:'Present Location'});
-    var from = new google.maps.Marker({position: muhanga, map: map,title:'Original location'});
-    var pickup = new google.maps.Marker({position: nyabugogo, map: map,title:'Pick up location'});
+    var present_location = new google.maps.Marker({position: kamonyi, map: map,title:'Present Location',label:'P'});
+    var from = new google.maps.Marker({position: muhanga, map: map,title:'Original location',icon:'../img/origin.png'});
+    var destination = new google.maps.Marker({position: nyabugogo, map: map,title:'Destination',label:'D'});
     }
